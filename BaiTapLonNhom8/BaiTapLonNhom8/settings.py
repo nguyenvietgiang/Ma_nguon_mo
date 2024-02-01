9eaa0a96b254260f43d89400d5bbd7cf082d6c01
Giang <nguyenvietgiang1110@gmail.com>
2024-02-01 11:30:40 +0700

    Dựng form

--- BaiTapLonNhom8/BaiTapLonNhom8/settings.py
+++ BaiTapLonNhom8/BaiTapLonNhom8/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nkiq621q7zrja#&4llj@_$y&-yjspbr2q2&r%!kxca3pxsal^q'
+SECRET_KEY = 'daylakhoabimatcuatoi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'register',
+    'login',
+    'blog',
 ]
 
 MIDDLEWARE = [
---
+++ README.md
@@ -0,0 +1,12 @@
+# Mã nguồn mở
+## Chạy dự án
+  **Khởi chạy server**: Trong thư mục dự án, chạy lệnh sau để khởi chạy server Django trên cổng 8888:
+
+    ```
+    python manage.py runserver 8888
+    ```
+---
+## Đăng nhập để quản lý CSDL với tài khoản: vietgiang, mật khẩu: vip1111.
+
+   Mở trình duyệt và truy cập địa chỉ [http://localhost:8888/](http://localhost:8888/).
+   Mở trình duyệt và truy cập địa chỉ trang quản trị [http://localhost:8888/admin](http://localhost:8888/admin). 
