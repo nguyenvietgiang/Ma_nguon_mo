f45f4d520c630d59c1e1cd03630a0fc8ce6752cd
Giang <nguyenvietgiang1110@gmail.com>
2024-03-08 20:43:07 +0700

    Cho phép xóa, sửa bài viết

--- BaiTapLonNhom8/blog/views.py
+++ BaiTapLonNhom8/blog/views.py
@@ -56,3 +56,28 @@ def add_comment(request, blog_id):
     # Chuyển hướng trở lại trang chi tiết blog sau khi thêm bình luận
     return redirect('blog_detail', blog_id=blog_id)
 
+@login_required(login_url='login') 
+def edit_blog(request, blog_id):
+    # Lấy bài viết cần chỉnh sửa từ ID
+    blog = get_object_or_404(Blog, id=blog_id, user=request.user)
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = BlogForm(instance=blog)
+    
+    return render(request, 'edit_blog.html', {'form': form})
+
+@login_required(login_url='login') 
+def delete_blog(request, blog_id):
+    # Lấy bài viết cần xóa từ ID
+    blog = get_object_or_404(Blog, id=blog_id, user=request.user)
+    
+    if request.method == 'POST':
+        blog.delete()
+        return redirect('profile')
+
+    return render(request, 'confirm_delete_blog.html', {'blog': blog})
